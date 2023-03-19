fd3293e200eb82a5dfd552e466f4a5aeb9e450fd
Ignat <petrov-ignat85@mail.ru>
2023-03-20 01:06:09 +0300

    updated for docker

--- backend/db_manager.py
+++ backend/db_manager.py
@@ -4,16 +4,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 # get data from environment
-# db_port = os.environ.get('POSTGRES_PORT') or 5432
-# db_host = os.environ.get('POSTGRES_HOST')
-# db_name = os.environ.get('POSTGRES_DB')
-# db_user = os.environ.get('POSTGRES_USER')
-# db_pass = os.environ.get('POSTGRES_PASSWORD')
-db_port = 5432
-db_host = "127.0.0.1"
-db_name = "Library"
-db_user = "postgres"
-db_pass = "qwerlodaza"
+db_port = os.environ.get('POSTGRES_PORT') or 5432
+db_host = os.environ.get('POSTGRES_HOST')
+db_name = os.environ.get('POSTGRES_DB')
+db_user = os.environ.get('POSTGRES_USER')
+db_pass = os.environ.get('POSTGRES_PASSWORD')
+# db_port = 5432
+# db_host = "127.0.0.1"
+# db_name = "Library"
+# db_user = "postgres"
+# db_pass = "qwerlodaza"
 
 SQLALCHEMY_DATABASE_URL = \
     f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
